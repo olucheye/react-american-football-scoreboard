dbfac7e0e9087b647b38481c678f42d0448ef935
olucheye <seyefree2rhyme@yahoo.com>
2020-06-12 19:43:26 +0100

    teamscore added to state and rendered in the DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, updateScore] = useState(32);
+  const [awayScore, updatedScore] = useState(32);
+
+  //changes state for homeScore
+  const homeTouch = () =>{
+      return updateScore(homeScore + 7);
+  }
+
+  //change state for awayScore
+  const awayTouch = ()=>{
+      return updatedScore(awayScore + 7);
+  }
+
+  //
 
   return (
     <div className="container">
@@ -14,12 +28,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,11 +42,11 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={homeTouch}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={awayTouch}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
